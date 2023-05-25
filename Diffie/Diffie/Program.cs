1394685f4626dc6afd0177a5d3e718e6e3766c21
Sean McArdle <sean@mcardletech.com>
2023-05-25 13:38:15 -0700

    Added governor on request body size.

--- Diffie/Diffie/Program.cs
+++ Diffie/Diffie/Program.cs
@@ -1,6 +1,7 @@
 using System.Dynamic;
 using System.Net;
 using JsonDiffer;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -8,6 +9,21 @@ using Newtonsoft.Json.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// Limit request body size
+const int sizeMb = 2;
+app.Use(async (context, next) =>
+{
+    var httpMaxRequestBodySizeFeature = context.Features.Get<IHttpMaxRequestBodySizeFeature>();
+    if (httpMaxRequestBodySizeFeature is not null) {
+        httpMaxRequestBodySizeFeature.MaxRequestBodySize = sizeMb * (int)Math.Pow(10, 6); // Compute size in bytes
+    } else {
+        throw new Exception("Cannot set request body limit!");
+    }
+    
+    await next(context);
+});
+
 
 app.MapPost("/", (List<dynamic> input) => {
     if (input.Count < 2)
